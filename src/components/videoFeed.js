13561c4d4f1c253d58e993c8c0eac4a56dbc2bba
Aissam Salhi <ais.salhy@gmail.com>
2017-06-10 12:47:52 +0200

    moving toward a correct structure. Only need to remove the button

--- src/components/videoFeed.js
+++ src/components/videoFeed.js
@@ -4,13 +4,18 @@ import ReactDOM from "react-dom";
 import VideoThumbnail from './videoThumbnail';
 
 const VideoFeed= (props) => {
-   var videoReceivedItems= props.videos.map( element => {
-   	  return <VideoThumbnail onSelectedVideo={props.onSelectedVideo} video={element} key={element.etag}/>;
+   var videoReceivedItems= props.videos.map( (element,index) => {
+   	  return <VideoThumbnail onSelectedVideo={props.onSelectedVideo} isLastElement={index===props.videos.length-1 ? true:false} video={element} key={element.etag}/>;
    });
 
-   return (<ul id="listOfVideos" className="col s12 m12 l5">
+   return ( <div id="listOfVideos" className="col s12 m12 l5 z-depth-3">
+   	<h5>Video list</h5>
+   	<div className="divider"></div>
+   	<ul>
    	{videoReceivedItems}
-   	</ul>);
+   	</ul>
+   	</div>
+   	);
 
 };
 
--- src/components/videoThumbnail.js
+++ src/components/videoThumbnail.js
@@ -6,18 +6,18 @@ var h6YoutubeStyle={
 };
 const VideoThumbnail = (props) => {
     	return (
-              <li className="row" onClick={() => {props.onSelectedVideo(props.video)}}>
-                <div className="valign-wrapper">
+              <li className="row searchResultItem" onClick={() => {props.onSelectedVideo(props.video)}}>
+                <div >
                   <div className="col s5 m5 l5 ">                  
                     <img className="responsive-img" src={props.video.snippet.thumbnails.medium.url} alt={props.video.snippet.title} />
                   </div>
                   <div className="col s7 m7 l7">
                     <h6 style={h6YoutubeStyle}>{props.video.snippet.title}</h6>
                     <p>{props.video.snippet.channelTitle} </p>
+                    
                   </div>
-
                 </div>
-                <div className="divider col s12 m12 l12"></div>
+                {props.isLastElement? "" : <div className="divider col s12 m12 l12"></div>}
               </li>
     		);
 
